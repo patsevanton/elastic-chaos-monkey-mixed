ee8583c33d51c06148f35810234dca59db64d059
Anton Patsev <patsev.anton@gmail.com>
2026-09-22 21:47:23 +0600

    увеличение памяти нод и ресурсов Elasticsearch

--- k8s.tf
+++ k8s.tf
@@ -86,7 +86,7 @@ resource "yandex_kubernetes_node_group" "k8s_node_group_a" {
 
     resources {
       cores  = 8
-      memory = 16
+      memory = 24
     }
 
     boot_disk {
@@ -126,7 +126,7 @@ resource "yandex_kubernetes_node_group" "k8s_node_group_b" {
 
     resources {
       cores  = 8
-      memory = 16
+      memory = 24
     }
 
     boot_disk {
@@ -166,7 +166,7 @@ resource "yandex_kubernetes_node_group" "k8s_node_group_d" {
 
     resources {
       cores  = 8
-      memory = 16
+      memory = 24
     }
 
     boot_disk {
